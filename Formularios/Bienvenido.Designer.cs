db9a855cdefe7ae90d0ab9db600cac732b535158
inbal <inbal@uma.es>
2023-12-18 19:51:44 +0100

    Formulario Bienvenido #2

--- Formularios/Bienvenido.Designer.cs
+++ Formularios/Bienvenido.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            okButton = new Button();
+            cancelButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            usuarioTextBox = new TextBox();
+            passwordTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // okButton
+            // 
+            okButton.Location = new Point(108, 356);
+            okButton.Name = "okButton";
+            okButton.Size = new Size(112, 34);
+            okButton.TabIndex = 0;
+            okButton.Text = "OK";
+            okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(345, 356);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(112, 34);
+            cancelButton.TabIndex = 1;
+            cancelButton.Text = "CANCEL";
+            cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(224, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(90, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(75, 230);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            // 
+            // usuarioTextBox
+            // 
+            usuarioTextBox.Location = new Point(184, 125);
+            usuarioTextBox.Name = "usuarioTextBox";
+            usuarioTextBox.Size = new Size(150, 31);
+            usuarioTextBox.TabIndex = 5;
+            // 
+            // passwordTextBox
+            // 
+            passwordTextBox.Location = new Point(184, 224);
+            passwordTextBox.Name = "passwordTextBox";
+            passwordTextBox.Size = new Size(150, 31);
+            passwordTextBox.TabIndex = 6;
+            // 
+            // Bienvenido
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(519, 450);
+            Controls.Add(passwordTextBox);
+            Controls.Add(usuarioTextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cancelButton);
+            Controls.Add(okButton);
+            Name = "Bienvenido";
             Text = "Hola";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button okButton;
+        private Button cancelButton;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox usuarioTextBox;
+        private TextBox passwordTextBox;
     }
 }
--- Formularios/Muestras.Designer.cs
+++ Formularios/Muestras.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Muestras";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(90, 371);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(90, 432);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(90, 483);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(90, 533);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 25);
+            label4.TabIndex = 3;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(458, 31);
+            label5.Name = "label5";
+            label5.Size = new Size(219, 54);
+            label5.TabIndex = 4;
+            label5.Text = "MUESTRAS";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(188, 533);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(864, 129);
+            listBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(188, 371);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(864, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(188, 432);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(864, 31);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(188, 483);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(864, 31);
+            textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(128, 754);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 9;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(388, 761);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 10;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(647, 746);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 11;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(917, 748);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 12;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(90, 112);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(962, 225);
+            dataGridView1.TabIndex = 13;
+            // 
+            // Muestras
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1154, 820);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Muestras";
+            Text = "Muestras";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
     }
 }
